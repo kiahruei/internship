a55977052b11a218e9a98c4cef1f11012024e215
kiahruei <kiahruei.how@student.unsw.edu.au>
2020-04-02 12:24:04 +0000

    internship proj

---
+++ Unemployment Rate in Singapore.R
@@ -0,0 +1,12 @@
+
+unemploy <- read_csv("unn rate.csv")
+
+pic <- ggplot(data=unemploy, aes(x = factor(year), y = unemployment_rate)) + 
+  geom_point(colour="tomato") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) +
+  xlab("Year") +
+  ylab("Unemployment Rate")
+
+pic <- pic + ggtitle ("Unemployment Rate Singapore")
+
+plot(pic)
